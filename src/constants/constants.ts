8a78320bc4ef9c974be212d10c1271e509c8954a
Kdrama Links <drvictorvondroom@gmail.com>
2025-07-15 21:16:04 -0700

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -33,33 +33,33 @@ export const HEADER_LINKS = [
 export const FAQS = [
 	{
 		id: 1,
-		header: 'What is Roseflix?',
+		header: 'What is Kdrama links?',
 		body:
-			"Roseflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single commercial – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!"
+			"Kdrama links is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single commercial – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!"
 	},
 	{
 		id: 2,
-		header: 'How much does Roseflix cost?',
+		header: 'How much does Kdrama links cost?',
 		body:
-			'Watch Roseflix on your smartphone, tablet, Smart TV, laptop or streaming device, all for one fixed monthly fee. Plans range from ₱149 to ₱549 a month. No extra costs, no contracts.'
+			'Watch Kdrama links on your smartphone, tablet, Smart TV, laptop or streaming device, all for one fixed monthly fee, However we will bring the free plan very sooner. Plans range from $2 to $80 a month. No extra costs, no contracts.'
 	},
 	{
 		id: 3,
 		header: 'Where can I watch?',
 		body:
-			"Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Roseflix account to watch instantly on the web at roseflix.com from your personal computer or on any internet-connected device that offers the Roseflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.\n\nYou can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Roseflix with you anywhere."
+			"Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Kdrama links account to watch instantly on the web at kdrama-links.com from your personal computer or on any internet-connected device that offers the Kdrama links app, including smart TVs, smartphones, tablets, streaming media players and game consoles.\n\nYou can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Kdrama-links with you anywhere."
 	},
 	{
 		id: 4,
 		header: 'How do I cancel?',
 		body:
-			'Roseflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.'
+			'Kdrama links is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.'
 	},
 	{
 		id: 5,
-		header: 'What can I watch on Roseflix?',
+		header: 'What can I watch on Kdrama links?',
 		body:
-			'Roseflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Roseflix originals, and more. Watch as much as you want, any time you want.'
+			'Kdrama links has an extensive library of Kdramas, feature films, documentaries, TV shows, anime, award-winning Kdrama links originals, and more. Watch as much as you want, any time you want.'
 	}
 ];
 
@@ -70,7 +70,7 @@ export const FEATURES = [
 		description: 'Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.',
 		video: '',
 		image: '/images/misc/Home-TV.jpg',
-		alt: 'Roseflix on TV'
+		alt: 'Kdrama links on TV'
 	},
 	{
 		id: 2,
@@ -78,7 +78,7 @@ export const FEATURES = [
 		description: 'Save your favorites easily and always have something to watch.',
 		video: '',
 		image: '/images/misc/Home-Mobile.jpg',
-		alt: 'Roseflix on mobile',
+		alt: 'Kdrama links on mobile',
 		direction: 'row-reverse'
 	},
 	{
@@ -86,7 +86,7 @@ export const FEATURES = [
 		title: 'Watch everywhere.',
 		description: 'Stream unlimited movies and TV shows on your phone, tablet, laptop and TV without paying more.',
 		image: '/images/misc/Home-IMac.jpg',
-		alt: 'Roseflix on different devices'
+		alt: 'Kdrama links on different devices'
 	}
 ];
 
@@ -107,5 +107,5 @@ export const FOOTER_LINKS = [
 	{ id: 14, text: 'Contact Us', url: 'https://help.netflix.com/contactus' },
 	{ id: 15, text: 'Speed Test', url: 'https://fast.com/' },
 	{ id: 16, text: 'Legal Notices', url: 'https://help.netflix.com/legal/notices' },
-	{ id: 17, text: 'Roseflix Originals', url: 'https://www.netflix.com/ph/browse/genre/839338' }
+	{ id: 17, text: 'Kdrama links Originals', url: 'https://www.netflix.com/ph/browse/genre/839338' }
 ];
